6e6c20109bb6882e3f1980d122832dbaa30eff89
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-12-28 05:38:59 +0530

    build

---
+++ cmake-build-debug/CMakeFiles/cmake-tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake-tutorial.dir/main.cpp.obj"
+  "CMakeFiles/cmake-tutorial.dir/main.cpp.obj.d"
+  "cmake-tutorial.exe"
+  "cmake-tutorial.exe.manifest"
+  "cmake-tutorial.lib"
+  "cmake-tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake-tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cmake-tutorial.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+﻿# Empty compiler generated dependencies file for cmake-tutorial.
+# This may be replaced when dependencies are built.
